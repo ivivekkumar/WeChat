5187aafeb703285b231697bcfb80efc7d000235d
ivivekkumar <ivivekkumar01@gmail.com>
2018-03-28 03:29:00 +0530

    Added UI for login and register

--- controllers/users.js
+++ controllers/users.js
@@ -5,10 +5,19 @@ module.exports = function(_){
     return{
         SetRouting: function(router){
             router.get('/', this.indexPage);
+            router.get('/signup', this.getSignup);
+
         },
 
         indexPage: function(req, res){
-            return res.render('index', {test: 'This is a test'});
+            return res.render('index');
+        },
+
+        getSignup: function(req, res){
+            return res.render('signup');
         }
+
     }
+
+        
 }
