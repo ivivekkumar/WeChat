feae281eec625ac525b96dcff72fbe8b6e7faf20
ivivekkumar <ivivekkumar01@gmail.com>
2018-03-29 03:15:21 +0530

    Added functionality for user login

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,16 @@
 'use strict';
 
-module.exports = function(_){
+module.exports = function(_, passport){
 
     return{
         SetRouting: function(router){
             router.get('/', this.indexPage);
             router.get('/signup', this.getSignup);
+            router.get('/home', this.homePage);
+
+
+
+            router.post('/signup', this.postSignup);
 
         },
 
@@ -15,6 +20,17 @@ module.exports = function(_){
 
         getSignup: function(req, res){
             return res.render('signup');
+        },
+
+        postSignup: passport.authenticate('local.signup', {
+            successRedirect: '/home',
+            failureRedirect: '/signup',
+            failureFlash: true
+        }),
+
+        homePage: function(req, res){
+            return res.render('home');
+            
         }
 
     }
